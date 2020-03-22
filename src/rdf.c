7451ab9de6efa55fe05f904bfa2c9542b90f9278
OddAbo <398774325@qq.com>
2020-03-23 04:48:24 +0800

    omp version

--- src/rdf.c
+++ src/rdf.c
@@ -3,23 +3,24 @@
  * Radial distribution function g(r)
  */
 
+#include <omp.h>
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
 
-#define gbox 8
-#define nbox 500
+#define rcut 10
+#define nbin 500
 
 #define vshell 4 * M_PI * (pow((i + 1) * dbox, 3) - pow(i * dbox, 3)) / 3.
 
 int main(int argc, char *argv[])
 {
   FILE *fp;
-  double *xyz;
+  double *xyz, gr[nbin] = {0};
   int nstep = 1, natom = 43, ipbc = 0, ibox, istep, i, j;
-  double gr[nbox] = {0};
-  double dbox, dx, dy, dz, dr, box2, pbc_box, scale = 1, rho = 1;
+  double dbox, pbc_x = 1, pbc_y = 1, pbc_z = 1;
+  double dx, dy, dz, dr, rho = 1;
 
   for (i = 1; i < argc; i += 2)
   {
@@ -29,9 +30,10 @@ int main(int argc, char *argv[])
       natom = atoi(argv[i+1]);
     } else if (strcmp(argv[i], "pbc") == 0) {
       ipbc = 1;
-      pbc_box = atof(argv[i+1]);
-      scale = 3 * pbc_box * pbc_box;  
-      rho = natom / pow(pbc_box, 3);
+      pbc_x = atof(argv[i+1]);
+      pbc_y = pbc_x;
+      pbc_z = pbc_x;
+      rho = natom / (pbc_x * pbc_y * pbc_z); 
     }
   }
 
@@ -40,9 +42,9 @@ int main(int argc, char *argv[])
     printf("Error: file 'md.out' does not exist!\n");
     exit(1);
   }
+
   xyz = (double *)malloc(sizeof(double) * 3 * natom);
-  dbox = (float)gbox / nbox;
-  box2 = gbox * gbox / scale;
+  dbox = (float)rcut / nbin;
 
   for (istep = 0; istep < nstep; ++istep)
   {
@@ -50,31 +52,37 @@ int main(int argc, char *argv[])
     fread(xyz, sizeof(double), 3 * natom, fp);
 
     /* Calculate distance */
+#pragma omp parallel for schedule(guided) \
+    private(j, dx, dy, dz, dr, ibox) firstprivate(dbox, ipbc, pbc_x, pbc_y, pbc_z)
     for (i = 0; i < natom - 1; ++i)
     {
       for (j = i + 1; j < natom; ++j)
       {
         dx = *(xyz + 3 * i) - *(xyz + 3 * j);
-        dy = *(xyz + 3 * i + 1) - *(xyz + 3 * j + 1); 
+        dy = *(xyz + 3 * i + 1) - *(xyz + 3 * j + 1);
         dz = *(xyz + 3 * i + 2) - *(xyz + 3 * j + 2);
 
         /* Periodic boundary condition */
         if (ipbc) {
-          if (dx > 0.5) dx -= 1;
-          else if (dx < -0.5) dx += 1; 
-          if (dy > 0.5) dy -= 1;
-          else if (dy < -0.5) dy += 1;
-          if (dz > 0.5) dz -= 1;
-          else if (dz < -0.5 * pbc_box) dz += 1; 
+          if (dx > 0.5) --dx;
+          else if (dx < -0.5) ++dx;
+          if (dy > 0.5) --dy;
+          else if (dy < -0.5) ++dy;
+          if (dz > 0.5) --dz;
+          else if (dz < -0.5) ++dz; 
+          dx *= pbc_x;
+          dy *= pbc_y;
+          dz *= pbc_z;
         }
 
         dr = dx * dx + dy * dy + dz * dz;
       }
 
-      if (dr > box2) continue;
-      dr = sqrt(dr * scale);
+      if (dr > rcut * rcut) continue;
+      dr = sqrt(dr);
       ibox = (int)(dr / dbox);
-      ++gr[ibox];
+#pragma omp critcal
+      ++ *(gr + ibox);
     }
   }
   free(xyz);
@@ -82,7 +90,7 @@ int main(int argc, char *argv[])
 
   /* Correlation and output */
   fp = fopen("rdf.dat","w");
-  for (i = 0; i < nbox; ++i)
+  for (i = 0; i < nbin; ++i)
   {
     gr[i] /= vshell * nstep * rho;
     fprintf(fp,"%-.3f   %-.6f\n", (i + 0.5) * dbox, gr[i]);
