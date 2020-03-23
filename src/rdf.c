6782c266b4b82377397fa8c6aea0ab6b03a16e15
OddAbo <398774325@qq.com>
2020-03-23 15:53:19 +0800

    set omp threads manually

--- src/rdf.c
+++ src/rdf.c
@@ -18,22 +18,24 @@ int main(int argc, char *argv[])
 {
   FILE *fp;
   double *xyz, gr[nbin] = {0};
-  int nstep = 1, natom = 43, ipbc = 0, ibox, istep, i, j;
+  int nthreads = 4, nstep = 1, natom = 43, ipbc = 0, ibox, istep, i, j;
   double dbox, pbc_x = 1, pbc_y = 1, pbc_z = 1;
-  double dx, dy, dz, dr, rho = 1;
+  double rcut2 = rcut * rcut, dx, dy, dz, dr, rho = 1;
 
   for (i = 1; i < argc; i += 2)
   {
     if (strcmp(argv[i], "nstep") == 0) {
-      nstep = atoi(argv[i+1]);
+      nstep = atoi(argv[i + 1]);
     } else if (strcmp(argv[i], "natom") == 0) {
-      natom = atoi(argv[i+1]);
+      natom = atoi(argv[i + 1]);
     } else if (strcmp(argv[i], "pbc") == 0) {
       ipbc = 1;
-      pbc_x = atof(argv[i+1]);
+      pbc_x = atof(argv[i + 1]);
       pbc_y = pbc_x;
       pbc_z = pbc_x;
       rho = natom / (pbc_x * pbc_y * pbc_z); 
+    } else if (strcmp(argv[i], "nthreads") == 0) {
+      nthreads = atoi(argv[i + 1]);
     }
   }
 
@@ -52,6 +54,7 @@ int main(int argc, char *argv[])
     fread(xyz, sizeof(double), 3 * natom, fp);
 
     /* Calculate distance */
+    omp_set_num_threads(nthreads);
 #pragma omp parallel for schedule(guided) \
     private(j, dx, dy, dz, dr, ibox) firstprivate(dbox, ipbc, pbc_x, pbc_y, pbc_z)
     for (i = 0; i < natom - 1; ++i)
@@ -64,21 +67,17 @@ int main(int argc, char *argv[])
 
         /* Periodic boundary condition */
         if (ipbc) {
-          if (dx > 0.5) --dx;
-          else if (dx < -0.5) ++dx;
-          if (dy > 0.5) --dy;
-          else if (dy < -0.5) ++dy;
-          if (dz > 0.5) --dz;
-          else if (dz < -0.5) ++dz; 
-          dx *= pbc_x;
-          dy *= pbc_y;
-          dz *= pbc_z;
+          if (dx > 0.5) dx = (dx - 1) * pbc_x;
+          else if (dx < -0.5) dx = (dx + 1) * pbc_x;
+          if (dy > 0.5) dy = (dy - 1) * pbc_y;
+          else if (dy < -0.5) dy = (dy + 1) * pbc_y;
+          if (dz > 0.5) dz = (dz - 1) * pbc_z;
+          else if (dz < -0.5) dz = (dz + 1) * pbc_z; 
         }
-
         dr = dx * dx + dy * dy + dz * dz;
       }
 
-      if (dr > rcut * rcut) continue;
+      if (dr > rcut2) continue;
       dr = sqrt(dr);
       ibox = (int)(dr / dbox);
 #pragma omp critcal
