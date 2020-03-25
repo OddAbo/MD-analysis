52857968861f388f74cc26a2814ea8958f91ae76
OddAbo <398774325@qq.com>
2020-03-25 13:34:02 +0800

    parallel

--- src/rdf.c
+++ src/rdf.c
@@ -4,23 +4,29 @@
  */
 
 #include <omp.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-#define rcut 10
+#define rcut 8
 #define nbin 500
-
+#define list_max 500
 #define vshell 4 * M_PI * (pow((i + 1) * dbox, 3) - pow(i * dbox, 3)) / 3.
+#define rlistshell 0.3 * rcut
+
+int natom, ilist, ipbc = 0, nthread = 4, \
+    nstep, ibin, istep, ifresh, ithread, i, j;
+double dr, sum_dr = 0, dbox = (double)rcut / nbin, \
+       rlistshell2 = (rcut + rlistshell) * (rcut + rlistshell), \
+       pbc_x = 1, pbc_y = 1, pbc_z = 1, dx, dy, dz, \
+       rcut2 = rcut * rcut, rho;
+int *list;
+double *xyz, *xyz_old, *gr, *dr_max;
 
 int main(int argc, char *argv[])
 {
   FILE *fp;
-  double *xyz, gr[nbin] = {0};
-  int nthread = 4, nstep = 1, natom = 43, ipbc = 0, ibox, istep, i, j;
-  double dbox, pbc_x = 1, pbc_y = 1, pbc_z = 1;
-  double rcut2 = rcut * rcut, dx, dy, dz, dr, rho = 1;
 
   for (i = 1; i < argc; i += 2)
   {
@@ -39,34 +45,130 @@ int main(int argc, char *argv[])
     }
   }
 
+  /* Check file status */
   if ((fp = fopen("md.out","rb")) == 0)
   {
     printf("Error: file 'md.out' does not exist!\n");
     exit(1);
   }
 
-  xyz = (double *)malloc(sizeof(double) * 3 * natom);
-  dbox = (float)rcut / nbin;
+  /* Check thread setting */
+  if (nthread > omp_get_max_threads())
+  {
+    printf("Error: require too many threads!\n");
+    exit(1);
+  }
+
+  /* Set how many threads will be used (4 by default) */
   omp_set_num_threads(nthread);
+
+  /* Initialize array */
+  dr_max = (double *)malloc(sizeof(double) * nthread);
+  gr = (double *)malloc(sizeof(double) * nbin * nthread);
+  list = (int *)malloc(sizeof(double) * list_max * natom);
+  xyz = (double *)malloc(sizeof(double) * 3 * natom);
+  xyz_old = (double *)malloc(sizeof(double) * 3 * natom);
+  memset(xyz, 0, sizeof(double) * 3 * natom);
+  memset(gr, 0, sizeof(double) * nbin * nthread);
 
   for (istep = 0; istep < nstep; ++istep)
   {
     /* Read coordinate of atoms */
+    memcpy(xyz_old, xyz, sizeof(double) * 3 * natom);
+    memset(dr_max, 0, sizeof(double) * (nthread + 1));
     fread(xyz, sizeof(double), 3 * natom, fp);
+
+    /* Calculate max displacement */
+#pragma omp parallel for schedule(guided) \
+        private(ithread, dx, dy, dz, dr) \
+        firstprivate(pbc_x, pbc_y, pbc_z, natom)
+    for (i = 0; i < natom; ++i)
+    {
+      ithread = omp_get_thread_num();
+
+      dx = *(xyz + 3 * i) - *(xyz_old + 3 * i);
+      dy = *(xyz + 3 * i + 1) - *(xyz_old + 3 * i + 1);
+      dz = *(xyz + 3 * i + 2) - *(xyz_old + 3 * i + 2);
+      if (ipbc)
+      {
+        if (dx > 0.5) --dx;
+        else if (dx < -0.5) ++dx;
+        if (dy > 0.5) --dy;
+        else if (dy < -0.5) ++dy;
+        if (dz > 0.5) --dz;
+        else if (dz < -0.5) ++dz;
+        dx *= pbc_x;
+        dy *= pbc_y;
+        dz *= pbc_z;
+      }
+      dr = sqrt(dx * dx + dy * dy + dz * dz);
+      *(dr_max + ithread) = (dr > *(dr_max + ithread)) ? \
+                             dr : *(dr_max + ithread);
+    }
+
+    for (ithread = 1; ithread < nthread; ++ithread)
+    {
+      *dr_max = (*(dr_max + ithread) > *dr_max) ? \
+                 *(dr_max + ithread) : *dr_max;
+    }
+    sum_dr += *dr_max;
+
+    /* Set neighbor list */
+    if (sum_dr > 0.5 * rlistshell) 
+    {
+      /* Reset sum of maximum diplacement, neighbor list */
+      sum_dr = 0;
+      memset(list, 0, sizeof(int) * list_max * natom);
+
+#pragma omp parallel for schedule(guided) \
+        private(j, dx, dy, dz, dr, ilist) \
+        firstprivate(natom, ipbc, pbc_x, pbc_y, pbc_z, rlistshell2)
+      for (i = 0; i < natom - 1; ++i)
+      {
+        ilist = 1;
+        for (j = i + 1; j < natom; ++j)
+        {
+          dx = *(xyz + 3 * i) - *(xyz + 3 * j);
+          dy = *(xyz + 3 * i + 1) - *(xyz + 3 * j + 1);
+          dz = *(xyz + 3 * i + 2) - *(xyz + 3 * j + 2);
+          if (ipbc)
+          {
+            if (dx > 0.5) --dx;
+            else if (dx < -0.5) ++dx;
+            if (dy > 0.5) --dy;
+            else if (dy < -0.5) ++dy;
+            if (dz > 0.5) --dz;
+            else if (dz < -0.5) ++dz;
+            dx *= pbc_x;
+            dy *= pbc_y;
+            dz *= pbc_z;
+          }
+          dr = dx * dx + dy * dy + dz * dz;
+          if (dr > rlistshell2) continue;
+
+          /* Store index of atom, store total numbe of index in list[i][0] */ 
+          *(list + i * list_max + ilist) = j;
+          ++ilist;
+        }
+        *(list + i * list_max) = ilist;
+      }
+    }
 
     /* Calculate distance */
 #pragma omp parallel for schedule(guided) \
-        private(j, dx, dy, dz, dr, ibox) firstprivate(dbox, ipbc, pbc_x, pbc_y, pbc_z)
+        private(ithread, ilist, j, dx, dy, dz, dr, ibin) \
+        firstprivate(dbox, ipbc, pbc_x, pbc_y, pbc_z)
     for (i = 0; i < natom - 1; ++i)
     {
-      for (j = i + 1; j < natom; ++j)
+      ithread = omp_get_thread_num();
+      for (ilist = 1; ilist < *(list + i * list_max); ++ilist)
       {
+        j = *(list + i * list_max + ilist);
         dx = *(xyz + 3 * i) - *(xyz + 3 * j);
         dy = *(xyz + 3 * i + 1) - *(xyz + 3 * j + 1);
         dz = *(xyz + 3 * i + 2) - *(xyz + 3 * j + 2);
-
-        /* Periodic boundary condition */
-        if (ipbc) {
+        if (ipbc)
+        {
           if (dx > 0.5) --dx;
           else if (dx < -0.5) ++dx;
           if (dy > 0.5) --dy;
@@ -78,24 +180,36 @@ int main(int argc, char *argv[])
           dz *= pbc_z;
         }
         dr = dx * dx + dy * dy + dz * dz;
+        if (dr > rcut2) continue;
+        dr = sqrt(dr);
+        ibin = (int)(dr / dbox);
+        ++ *(gr + ithread * nbin + ibin);
       }
-
-      if (dr > rcut2) continue;
-      dr = sqrt(dr);
-      ibox = (int)(dr / dbox);
-#pragma omp critcal
-      ++ *(gr + ibox);
     }
   }
+
+  /* Sum g(r) */
+#pragma omp parallel for schedule(guided) \
+        private(ithread)
+  for (ibin = 0; ibin < nbin; ++ibin)
+  {
+    for (ithread = 1; ithread < nthread; ++ithread)
+    {
+      *(gr + ibin) += *(gr + ithread * nbin + ibin);
+    }
+  }
+
   free(xyz);
+  free(xyz_old);
+  free(list);
   fclose(fp);
 
-  /* Correlation and output */
+  /* Normalize and output */
   fp = fopen("rdf.dat","w");
   for (i = 0; i < nbin; ++i)
   {
-    gr[i] /= vshell * nstep * rho;
-    fprintf(fp,"%-.3f   %-.6f\n", (i + 0.5) * dbox, gr[i]);
+    *(gr + i) /= 0.5 * natom * vshell * nstep * rho;
+    fprintf(fp,"%-.3f   %-.6f\n", (i + 0.5) * dbox, *(gr + i));
   }
   fclose(fp);
 
